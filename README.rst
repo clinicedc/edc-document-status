dbae8e367c5b5af7f68f11e7daa684a537f50ccb
erikvw <ew2789@gmail.com>
2023-12-14 20:13:03 -0600

    test setup

--- README.rst
+++ README.rst
@@ -6,8 +6,8 @@ edc_document_status
 .. |pypi| image:: https://img.shields.io/pypi/v/edc-document-status.svg
     :target: https://pypi.python.org/pypi/edc-document-status
 
-.. |actions| image:: https://github.com/clinicedc/edc-document-status/workflows/build/badge.svg?branch=develop
-  :target: https://github.com/clinicedc/edc-document-status/actions?query=workflow:build
+.. |actions| image:: https://github.com/clinicedc/edc-document-status/actions/workflows/build.yml/badge.svg
+  :target: https://github.com/clinicedc/edc-document-status/actions/workflows/build.yml
 
 .. |codecov| image:: https://codecov.io/gh/clinicedc/edc-document-status/branch/develop/graph/badge.svg
   :target: https://codecov.io/gh/clinicedc/edc-document-status
--- runtests.py
+++ runtests.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
 import logging
-import os
-import sys
-from os.path import abspath, dirname
+from pathlib import Path
 
-import django
-from django.conf import settings
-from django.test.runner import DiscoverRunner
-from edc_test_utils import DefaultTestSettings
+from edc_test_utils import DefaultTestSettings, func_main
 
 app_name = "edc_document_status"
-base_dir = dirname(abspath(__file__))
+base_dir = Path(__file__).absolute().parent
 
-DEFAULT_SETTINGS = DefaultTestSettings(
+project_settings = DefaultTestSettings(
     calling_file=__file__,
     BASE_DIR=base_dir,
     APP_NAME=app_name,
-    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
     SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
     INSTALLED_APPS=[
         "django.contrib.admin",
@@ -29,23 +23,6 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "django_crypto_fields.apps.AppConfig",
         "django_revision.apps.AppConfig",
         "multisite",
-        # "edc_action_item.apps.AppConfig",
-        # "edc_appointment.apps.AppConfig",
-        # "edc_consent.apps.AppConfig",
-        # "edc_device.apps.AppConfig",
-        # "edc_export.apps.AppConfig",
-        # "edc_facility.apps.AppConfig",
-        # "edc_identifier.apps.AppConfig",
-        # "edc_metadata.apps.AppConfig",
-        # "edc_notification.apps.AppConfig",
-        # "edc_protocol.apps.AppConfig",
-        # "edc_registration.apps.AppConfig",
-        # "edc_sites.apps.AppConfig",
-        # "edc_timepoint.apps.AppConfig",
-        # "edc_visit_schedule.apps.AppConfig",
-        # "visit_schedule_app.apps.AppConfig",
-        # "edc_visit_tracking.apps.AppConfig",
-        # "edc_crf.apps.AppConfig",
         "edc_document_status.apps.AppConfig",
     ],
     add_dashboard_middleware=True,
@@ -53,13 +30,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
 
 
 def main():
-    if not settings.configured:
-        settings.configure(**DEFAULT_SETTINGS)
-    django.setup()
-    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
-    failfast = True if [t for t in sys.argv if t == "--failfast"] else False
-    failures = DiscoverRunner(failfast=failfast, tags=tags).run_tests([f"{app_name}.tests"])
-    sys.exit(bool(failures))
+    func_main(project_settings, f"{app_name}.tests")
 
 
 if __name__ == "__main__":
